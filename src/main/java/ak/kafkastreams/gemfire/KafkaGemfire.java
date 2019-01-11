892aaf61ae3e55aa28cca72d7686e14529fca308
ash26389 <ash26389@google.com>
2019-01-10 23:59:27 -0500

    AK | Initial commit

---
+++ src/main/java/ak/kafkastreams/gemfire/KafkaGemfire.java
@@ -0,0 +1,11 @@
+package ak.kafkastreams.gemfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaGemfire {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaGemfire.class, args);
+    }
+}
